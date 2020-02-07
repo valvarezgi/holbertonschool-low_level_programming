bbca9a256a4dee95b2d9762cfc1c335c69e5213d
valvarezgi <vale.agiraldo@gmail.com>
2020-02-07 00:37:13 +0000

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 100; n++)
+{
+putchar((n / 10) + '0');
+putchar((n % 10) + '0');
+if (n != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+return (0);
+}
