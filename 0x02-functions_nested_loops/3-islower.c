8b718634fb1eb2295773713752b401e26ea05fd4
valvarezgi <vale.agiraldo@gmail.com>
2020-02-11 01:05:17 +0000

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * int _islower(int c) - checks for lowercase character.
+ *
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	return (1);
+	else
+	return (0);
+
