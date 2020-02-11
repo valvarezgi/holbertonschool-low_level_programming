ca07a2f0b7c7b4adf3d75f4eec20e101f8723eab
valvarezgi <vale.agiraldo@gmail.com>
2020-02-10 22:04:32 -0500

    C-Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_abs - function that computes the absolute value of a integer
+ *
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (0 -a);
+	}
+}
