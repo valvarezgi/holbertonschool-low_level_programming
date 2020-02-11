12b3eccc4e68eb54e9919b59d2a4916f20e4cbf4
valvarezgi <vale.agiraldo@gmail.com>
2020-02-11 17:14:45 +0000

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - is a function that adds two integers
+ * and returns the result
+ * @n: is a number
+ * @m: is a number
+ * Return: add between n and m
+ */
+
+int add(int n, int m)
+{
+	int add;
+
+	add = n + m;
+	return (add);
+}
