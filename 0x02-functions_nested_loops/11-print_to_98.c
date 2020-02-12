5a547554ae8fe6c64ec138bba6f55e8b2c808f3e
valvarezgi <vale.agiraldo@gmail.com>
2020-02-11 20:28:30 -0500

    C-Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,33 +8,34 @@
  */
 void print_to_98(int n)
 {
-if (n < 98)
-{
-while (n <= 98)
-{
-printf("%d", n);
-if (n != 98)
-{
-printf(", ");
-}
-n++;
-}
-}
-else if (n > 98)
-{
-while (n >= 98)
-{
-printf("%d", n);
-if (n != 98)
-{
-printf(", ");
-}
-n--;
-}
-}
-else
-{
-printf("98");
-}
-printf("\n");
+	int a, b, c, d;
+
+	while (n != 98)
+	{
+		a = abs(n), b = a / 100, c = a / 10 % 10, d = a % 10;
+		if (b > 0)
+		{
+			a != n ? _putchar('-') : a;
+			_putchar(b + '0');
+			_putchar(c + '0');
+			_putchar(d + '0');
+		}
+		else if (c > 0)
+		{
+			a != n ? _putchar('-') : a;
+			_putchar(c + '0');
+			_putchar(d + '0');
+		}
+		else
+		{
+			a != ? _putchar('-') : a;
+			_putchar(d + '0');
+		}
+		_putchar(',');
+		_putchar(' ');
+		n < 98 ? n++ : n--;
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
 }
