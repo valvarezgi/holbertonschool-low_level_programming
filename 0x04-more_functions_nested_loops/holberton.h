aba959c7068e5fe29a5fc715b4d0cdfe84203b34
valvarezgi <vale.agiraldo@gmail.com>
2020-02-13 18:02:36 +0000

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,9 +3,8 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b)
 
 #endif /* HOLBERTON_H */
