a87909ada8db5bba96af712cdadf043f99e64013
valvarezgi <vale.agiraldo@gmail.com>
2020-02-13 20:32:32 -0500

    C-More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			if (n % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else
+			{
+				if (n % 5 == 0)
+				{
+					printf("Buzz");
+				}
+				else
+				{
+					printf("%d", n);
+				}
+			}
+		}
+		if (n != 0)
+		{
+			printf(" ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
