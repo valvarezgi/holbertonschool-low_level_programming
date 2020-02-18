a4fa42713f0cea547a976f0ffdf3674498801ea8
valvarezgi <vale.agiraldo@gmail.com>
2020-02-18 16:03:01 +0000

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
 
-Coments
+/**
+ * print_rev -  function that prints a string, in reverse
+ * @s: sis a string
+ */
 
 void print_rev(char *s)
 {
-  int i = 0;
+	int i = 0;
 
-  while (s[i] != 0)
-    i++;
+	while (s[i] != 0)
+		i++;
 
-  for (i--; i >= 0; i--)
-    _putchar(s[i]);
-  _putchar('\n');
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
