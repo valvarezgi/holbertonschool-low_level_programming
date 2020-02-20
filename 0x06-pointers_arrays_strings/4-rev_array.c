cb69eef23fac17c308516f41e1d008b64c1eba8b
valvarezgi <vale.agiraldo@gmail.com>
2020-02-20 22:54:10 +0000

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - function that reverses the content of an array of integers
+ * @a: is the array
+ * @n: is the number of elements of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temporal;
+
+	i = 0;
+	n = n - 1;
+
+	while(i <= n)
+	{
+		temporal = a[i];
+		a[i] = a[n];
+		a[n] = temporal;
+		i++;
+	}
+	n++;
+}
