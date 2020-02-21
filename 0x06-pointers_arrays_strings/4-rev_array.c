6a5c1b8171babbaba8640e8c53345102e238218a
valvarezgi <vale.agiraldo@gmail.com>
2020-02-20 22:24:51 -0500

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,7 +4,7 @@
  * reverse_array - function that reverses the content of an array of integers
  * @a: is the array
  * @n: is the number of elements of the array
- * Returns: a reverse array 
+ * Returns: a reverse array
  */
 
 void reverse_array(int *a, int n)
@@ -14,7 +14,7 @@ int temporal;
 
 i = 0;
 n = n - 1;
-while(i <= n)
+while (i <= n)
 {
 temporal = a[i];
 a[i] = a[n];
