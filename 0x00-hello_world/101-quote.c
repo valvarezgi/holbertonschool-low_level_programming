9d42ebaf063d646e81659319c5022018c9f1f0e7
valvarezgi <vale.agiraldo@gmail.com>
2020-02-23 20:21:07 -0500

    C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - program that prints a quote to the standard error
+ *Return: 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n,58)
+		return (1);
+}
