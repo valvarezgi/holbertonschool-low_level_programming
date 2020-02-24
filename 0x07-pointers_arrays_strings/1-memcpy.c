3c574ce67ea2eb092a177827f3d59db895372755
valvarezgi <vale.agiraldo@gmail.com>
2020-02-24 18:09:19 +0000

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _memcpy - function that copies memory area
+ * @des: memory area
+ * @src: memory area
+ * @n: number of bytes
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+  unsigned int i;
+
+  for(i = 0; i < n; i++)
+
+    dest[i] = src[i];
+
+  return (dest);
+}
