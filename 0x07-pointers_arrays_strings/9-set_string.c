9ba30b1a87e95e5881dbc16e80ee5381b7d52ef2
valvarezgi <vale.agiraldo@gmail.com>
2020-02-24 22:39:33 -0500

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 
-/** set_string - function that sets the value of a pointer to a char
+/**
+ *set_string - function that sets the value of a pointer to a char
  * @s : double pointer to set
  * @to: the char to point to
  */
