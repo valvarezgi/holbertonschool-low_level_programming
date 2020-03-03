700915aaa468944d091c013817010ca7cf0a1246
valvarezgi <vale.agiraldo@gmail.com>
2020-03-03 18:45:15 +0000

    C- malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,8 @@ char *create_array(unsigned int size, char c)
 {
 	char *ps;
 	unsigned int i;
-	
-	ps = malloc(sizeof (char) * size);
+
+	ps = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		ps[i] = c;
 	ps[i] = 0;
