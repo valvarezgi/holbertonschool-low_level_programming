236ead7cbf53ea73251c0dd045bc83ef2f1d2839
valvarezgi <vale.agiraldo@gmail.com>
2020-03-03 21:01:34 -0500

    C - malloc, free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 
 /**
  * alloc_grid - function that returns a pointer to s 2 dimensional array of int
@@ -11,5 +11,39 @@
 
 int **alloc_grid(int width, int height)
 {
+	int h, w;
+	int *p;
+	int **dp;
 
+	if (width < 1 || height < 1)
+		return (NULL);
+	dp = malloc(sizeof(int *) * height);
+	if (dp != NULL)
+	{
+		for (h = 0; h < height; h++)
+		{
+			p = malloc((sizeof(int) * width));
+			if (p != NULL)
+			{
+				dp[h] = p;
+				for (w = 0; w < width; w++)
+				{
+					p[w] = 0;
+				}
+			}
+
+			else
+			{
+				h--;
+				while (h >= 0)
+				{
+					free(dp[h]);
+					h--;
+				}
+				free(dp);
+				return (NULL);
+			}
+		}
+	}
+	return (dp);
 }
