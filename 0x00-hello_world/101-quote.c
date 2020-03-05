fe22e3437dec22c7a144712adcc323789aba9fd1
valvarezgi <vale.agiraldo@gmail.com>
2020-03-05 18:47:10 -0500

    Fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  *main - program that prints a quote to the standard error
@@ -8,6 +7,6 @@
 
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 		return (1);
 }
