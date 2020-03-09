9dd8ddad6426cec8bc434e3f7da94cad8c195992
valvarezgi <vale.agiraldo@gmail.com>
2020-03-09 18:06:59 +0000

    C - Preprocessor

---
+++ 0x0D-preprocessor/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int findSquare(int n, int i);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int checkPalindrome(int n, int i, char *s);
+
+#endif
