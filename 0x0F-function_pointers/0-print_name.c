c1e2b8c0dde1a474581195f4c433a3a61e9e50a4
valvarezgi <vale.agiraldo@gmail.com>
2020-03-11 18:30:05 +0000

    C - Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+ /**
+ * print_name - function that prints a name
+ * @name: name of the person
+ * @f: function to call to print the name 
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+  (*f)(name);
+}
