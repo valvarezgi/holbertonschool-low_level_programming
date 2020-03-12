b357515c17397e6ad79c29046633d1e50a52d66d
valvarezgi <vale.agiraldo@gmail.com>
2020-03-11 21:31:34 -0500

    Add main task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-cal.h"
+
+/**
+ * main - principal function
+ * @argc: arguments counter
+ * @argv: array of arguments
+ *Return: 0 or a number
+ */
+int main(int argc, char *argv[])
+{
+
+}
