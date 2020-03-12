ed1d946a06a33f86c91fe2f2510cbb5d9cfc4dbd
valvarezgi <vale.agiraldo@gmail.com>
2020-03-11 21:21:43 -0500

    Add files task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+/**
+ * op_add - add operation
+ * @a: number 1
+ * @b: number 2
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return(a + b);
+}
+/**
+ * op_sub - substracts operation
+ * @a: number 1
+ * @b: number 2
+ * Return: result
+ */
+int op_sub(ins a, int b)
+{
+	return(a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: number 1
+ * @b: number 2
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: number 1
+ * @b: number 2
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modules
+ * @a: number 1
+ * @b: number 2
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
