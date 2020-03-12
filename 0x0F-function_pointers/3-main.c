a210dfd3b0cc6dd192af2c73f7b5454e1248022f
valvarezgi <vale.agiraldo@gmail.com>
2020-03-12 18:12:27 -0500

    Add main task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-cal.h"
+#include "3-calc.h"
 
 /**
  * main - principal function
@@ -10,5 +10,22 @@
  */
 int main(int argc, char *argv[])
 {
-
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return(98);
+}
+	if ((*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
+	     && *argv[2] != '/' && *argv[2] != '%') || argv[2][1] != 0)
+	{
+		printf("Error\n");
+		return(99);
+}
+	if (atoi(argv[3]) == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		return(100);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
