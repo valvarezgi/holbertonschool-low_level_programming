c042c8f7cd82258530a90cffb6ac121e2eda6790
valeagiraldo <vale.agiraldo@gmail.com>
2020-03-14 16:32:38 +0000

    Adding tasks 0 and 1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: num of arguments, constant
+ * @...: arguments to sum
+ * Return: result
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int sum;
+
+	va_start(ap, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0, sum = 0; i < n; i++)
+		sum += va_arg(ap, int);
+			va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: numbers separator
+ * @n: number of arguments
+ * @...: arguments to sum
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (i != n - 1)
+				printf("%d%s", va_arg(ap, int), separator);
+			else
+				printf("%d", va_arg(ap, int));
+		}
+		printf("\n");
+	}
+}
