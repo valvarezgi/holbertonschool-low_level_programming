0af77603bcea9fa25195618dac720579de67d94a
valeagiraldo <vale.agiraldo@gmail.com>
2020-03-15 15:30:46 +0000

    Betty fixed task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,11 +11,11 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-        va_list ap;
-        unsigned int i;
+	va_list ap;
+	unsigned int i;
 	char *cptr;
 
-        va_start(ap, n);
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		cptr = va_arg(ap, char *);
