30307575ab588290883b33e77f15f91aff9f039a
valeagiraldo <vale.agiraldo@gmail.com>
2020-03-15 12:47:22 +0000

    Adding tasks 0 and 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,14 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-		for (i = 0; i < n; i++)
-		{
-			if (i != n - 1)
-				printf("%d%s", va_arg(ap, int), separator);
-			else
-				printf("%d", va_arg(ap, int));
-		}
-		va_end(ap);
-		printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i < n -1 && separator != NULL)
+			printf("%s", separator);
 	}
+	va_end(ap);
+	printf("\n");
 }
