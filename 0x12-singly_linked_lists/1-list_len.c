55503849869cd1c641a8acaddb11ce22a67b9ca7
valeagiraldo <vale.agiraldo@gmail.com>
2020-03-19 23:07:43 +0000

    Adding task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements of list_t
+ * @h: head
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h);
+{
+	list_t *lis;
+	size_t i = 0;
+
+	lis = (list_t *)h;
+	while (h != NULL && lis = NULL)
+	{
+
+		i++;
+		e = e->next;
+	}
+	return(i);
+}
