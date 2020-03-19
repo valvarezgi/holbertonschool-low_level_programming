fe1865c763377e30f0c22339c26e22a8b1c33a88
valeagiraldo <vale.agiraldo@gmail.com>
2020-03-19 23:16:45 +0000

    Fixing task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,17 +6,17 @@
  * Return: number of nodes
  */
 
-size_t list_len(const list_t *h);
+size_t list_len(const list_t *h)
 {
 	list_t *lis;
 	size_t i = 0;
 
 	lis = (list_t *)h;
-	while (h != NULL && lis = NULL)
+	while (h != NULL && lis != NULL)
 	{
 
 		i++;
-		e = e->next;
+		lis = lis->next;
 	}
 	return(i);
 }
