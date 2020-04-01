c4d1166300bd4a37fc6c93b3b29eb930754a270a
valvarezgi <vale.agiraldo@gmail.com>
2020-04-01 00:16:56 +0000

    Add task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file
+ * @text_content: Content of the file
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t cant;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd <= -1)
+		return (-1);
+
+	while (text_content != NULL && *text_content != '\0')
+	{
+		cant = write(fd, text_content, 1);
+		if (cant < 1)
+			return (-1);
+		text_content++;
+	}
+	if (close(fd) == -1)
+		return (-1);
+	return (1);
+}
