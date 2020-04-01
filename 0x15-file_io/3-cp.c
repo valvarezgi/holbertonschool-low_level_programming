540cafec5b3b1b587abbcbc96e4128983f7fc7ef
valvarezgi <vale.agiraldo@gmail.com>
2020-04-01 04:34:28 +0000

    Fixed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,42 +1,48 @@
 #include "holberton.h"
 /**
- * main - function that coies the content of one file to another
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 if success
+ * main - check the code for Holberton School students.
+ * @argc: num of args
+ * @argv: args
+ * Return: Always 0.
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int cantCp, cantO, cantO2;
-	char *buffer;
+	int size_to, size_from, file_to, file_from, close_file;
+	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage cp file_from file_to\n");
-			exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	buffer = malloc(1024 * sizeof(char));
-	if (buffer == NULL)
-		return (-1);
-
-	cantO = open(argv[1], O_RDONLY);
-	if (cantO == -1)
-	{
-		free(buffer);
-	  dprintf(STDERR_FILENO, "Error: Can\'t read from file %s\n", argv[1]);
-		   exit(98);
+	size_from = read(file_from, buf, 1024);
+	if (size_from == -1)
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	cantO2 = open(argv[2], O_RDWR | O_TRUNC);
-	if (cantO2 == -1)
+	size_to = write(file_to, buf, size_from);
+	if (size_to == -1)
 	{
-		close(cantO);
-		free(buffer);
-		dprintf(STDERR_FILENO, "Error: Can\'t write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	(void) cantCp;
-
-	free(buffer);
+	while (size_from == 1024)
+	{
+		size_from = read(file_from, buf, 1024);
+		size_to = write(file_to, buf, size_from);
+	}
+	/* close file */
+	close_file = close(file_from);
+	if (close_file == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	close_file = close(file_to);
+	if (close_file == -1)
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	}
 	return (0);
 }
