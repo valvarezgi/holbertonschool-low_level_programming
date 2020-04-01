fc81480d8ee2e294de4f2647733fbf98ccda4de6
valvarezgi <vale.agiraldo@gmail.com>
2020-04-01 00:22:59 +0000

    Betty issues fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,6 +3,7 @@
  * create_file - function that creates a file
  * @filename: name of the file
  * @text_content: Content of the file
+ * Return: 1 on success otherwise -1
  */
 
 int create_file(const char *filename, char *text_content)
