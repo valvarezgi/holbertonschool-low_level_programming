0b1f7ad32208bce29eee97791a6871ad18b9cf9c
valvarezgi <vale.agiraldo@gmail.com>
2020-05-02 22:32:26 +0000

    Task 3: handle NULL

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,24 +8,27 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *newNode, *aux;
+	dlistint_t *newNode, *aux;
 
-    newNode = malloc(sizeof(dlistint_t));
-    newNode->n = n;
-    newNode->next = NULL;
+	newNode = malloc(sizeof(dlistint_t));
+	if (!newNode)
+		return (NULL);
 
-    aux = *head;
-    if(aux)
-    {
-        while(aux->next)
-        aux = aux->next;
-        aux->next = newNode;
-        newNode->prev = aux;
-    }
-    else
-    {
-        newNode->prev = NULL;
-        *head = newNode;
-    }
-    return(newNode);
+	newNode->n = n;
+	newNode->next = NULL;
+
+	aux = *head;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+		aux->next = newNode;
+		newNode->prev = aux;
+	}
+	else
+	{
+		newNode->prev = NULL;
+		*head = newNode;
+	}
+	return (newNode);
 }
