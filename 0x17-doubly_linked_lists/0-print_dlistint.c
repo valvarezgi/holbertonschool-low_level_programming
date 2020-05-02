ee604770097cba3df43f20fcfef15c3749838618
valvarezgi <vale.agiraldo@gmail.com>
2020-05-02 16:55:19 +0000

    Task 0:function that prints all the elements of a list, added, also README

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - print the elements of a linked list
+ * @h: head
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t nCount = 0;
+
+    while(h)
+    {
+        printf("%d\n", h->n);
+        nCount++;
+        h = h->next;
+    }
+    return(nCount);
+}
