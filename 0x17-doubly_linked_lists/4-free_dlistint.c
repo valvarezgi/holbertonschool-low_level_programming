1d5d357ab128925e71137a7cc74dc260a98dc576
valvarezgi <vale.agiraldo@gmail.com>
2020-05-02 22:53:11 +0000

    Task 4: Function thta free a doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head:head of the list
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *aux;
+
+	while(head)
+	{
+		aux = head;
+		free(aux);
+		head = head->next;
+	}
+}
