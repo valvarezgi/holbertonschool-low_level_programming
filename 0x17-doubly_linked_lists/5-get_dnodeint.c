ce8432860f009039415d500a61cb145fee8433b5
valvarezgi <vale.agiraldo@gmail.com>
2020-05-03 14:37:11 +0000

    Task 5: Function that returns a node of a linked list, first version

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets data node at the index
+ * @head: head of the list
+ * @index: index given
+ *
+ * Return: node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    dlistint_t *aux;
+    unsigned int i;
+
+    aux = head;
+    for(i = 0; aux; aux = aux->next, i++)
+    {
+        if(i == index)
+            return(aux);
+    }
+    return(0);
+}
