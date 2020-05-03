4c88868c257b04dc8765415b5da9e415e19684a0
valvarezgi <vale.agiraldo@gmail.com>
2020-05-03 15:10:02 +0000

    Task 6: function that returns the sum of the data

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function that returns the sum of the data nodes
+ * @head: head of the list
+ * Return: sum of data nodes
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    int sum = 0;
+
+    while(head != NULL)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+    return (sum);
+}
