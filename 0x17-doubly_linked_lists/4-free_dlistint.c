42594ff507fa1edd422daa71efed916e233b998a
valvarezgi <vale.agiraldo@gmail.com>
2020-05-03 14:51:50 +0000

    Betty issues fixed

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -3,19 +3,20 @@
 /**
  * free_dlistint - frees a doubly linked list
  * @head:head of the list
- *
+ * Return: none
  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *aux;
 
-	if(!head)
+	if (!head)
 		return;
 
 	while (head)
 	{
 		aux = head;
 		head = head->next;
-		free(aux);	
+		free(aux);
 	}
+	free(head);
 }
