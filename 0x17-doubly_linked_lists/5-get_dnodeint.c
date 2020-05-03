2df1ec9f5020d926460fa577991e201688b0e1e7
valvarezgi <vale.agiraldo@gmail.com>
2020-05-03 14:40:28 +0000

    Task 5: Betty issues fixed

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,14 +9,14 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    dlistint_t *aux;
-    unsigned int i;
+	dlistint_t *aux;
+	unsigned int i;
 
-    aux = head;
-    for(i = 0; aux; aux = aux->next, i++)
-    {
-        if(i == index)
-            return(aux);
-    }
-    return(0);
+	aux = head;
+	for (i = 0; aux; aux = aux->next, i++)
+	{
+		if (i == index)
+			return (aux);
+	}
+	return (0);
 }
