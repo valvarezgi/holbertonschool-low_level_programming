04d71208729192ed3d535d00461f773c49744bd4
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-05-29 15:15:30 -0500

    [Fix] Style issues fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL)
 		return (0);
-	
+
 	idx = key_index((unsigned char *)key, ht->size);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
