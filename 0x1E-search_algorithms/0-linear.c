905ee86eb9762a2b740b170a0c4817dc5691e797
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-26 15:51:38 -0500

    Task 0: Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,15 +14,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
 
-	if (array)
+	if (!array)
+		return (-1);
+	for (index = 0; index < size; index++)
 	{
-		for (index = 0; index < size; index++)
-		{
-			printf("Value checked array[%lu] =[%d]\n",
-			       index, array[index]);
-			if (array[index] == value)
-				return (index);
-		}
+		printf("Value checked array[%lu] =[%d]\n", index, array[index]);
+		if (array[index] == value)
+			return (index);
 	}
 	return (-1);
 }
